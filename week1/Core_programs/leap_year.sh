e5bc3f9e72f1373808096987175f49bd15b0f1ff
kishorekumar646 <kishorekumar131646@gmail.com>
2020-02-07 16:12:30 +0530

    [kishore]Refactor.refactor use expr keyword

--- week1/Core_programs/leap_year.sh
+++ week1/Core_programs/leap_year.sh
@@ -5,7 +5,7 @@ if [ $(($year % 4)) -eq 0 ]
 then
 	if [ $(($year % 100)) -eq 0 ]
 	then
-		if [ $(($year % 400)) -eq 0 ]
+		if [ `expr $year % 400` -eq 0 ]
 		then
 			echo "$year is leap year"
 		else
