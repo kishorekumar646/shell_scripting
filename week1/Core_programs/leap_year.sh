8b1a64a84245d551553161b9c25b4f50523baa74
kishorekumar646 <kishorekumar131646@gmail.com>
2020-02-07 15:47:34 +0530

    [kishore]Add.added add statements

--- week1/Core_programs/leap_year.sh
+++ week1/Core_programs/leap_year.sh
@@ -3,7 +3,17 @@
 read -p "Enter the year : " year
 if [ $(($year % 4)) -eq 0 ]
 then
-	echo "$year is leap year"
+	if [ $(($year % 100)) -eq 0 ]
+	then
+		if [ $(($year % 400)) -eq 0 ]
+		then
+			echo "$year is leap year"
+		else
+			echo "$year is not leap year"
+		fi
+	else
+		echo "$year is leap year"
+	fi
 else
 	echo "$year is not leap year"
 fi
