23014f9e7769de8bcc07fac320dff3d12babc581
kishorekumar646 <kishorekumar131646@gmail.com>
2020-02-08 11:41:28 +0530

    [kishore]Refactor.refactor add method called distance

--- week1/Functional_programs/Distance.sh
+++ week1/Functional_programs/Distance.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+distance()
+{
+	c=$(echo "scale=3;sqrt ( $((x ** 2 + y ** 2)) )" | bc -l)
+	echo "$c"
+}
+
 read -p "Value of x : " x
 read -p "Value of y : " y
 
-
-c=$(echo "scale=3;sqrt ( $((x ** 2 + y ** 2)) )" | bc -l)
-
-echo "$c"
+distance $x $y
