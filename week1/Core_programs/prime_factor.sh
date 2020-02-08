ba2bcab69284ac82fb763f8ef27f50f17bb11de4
kishorekumar646 <kishorekumar131646@gmail.com>
2020-02-08 11:21:40 +0530

    [kishore]Refactor.refactor names of variables

--- week1/Core_programs/prime_factor.sh
+++ week1/Core_programs/prime_factor.sh
@@ -3,13 +3,13 @@
 read -p "Enter the number : " number
 
 while [[ $((number % 2)) -eq 0 ]]; do
-	echo "2"
-	number=$((number / 10))
+	echo -e "2"
+	number=$((number / 2))
 done
 
 for (( i = 3; i <=$(echo "sqrt ( $number )" | bc) ; i = i + 2)); do
 	while [[ $((number % i)) -eq 0 ]]; do
-		echo "$i"
+		echo -e "$i"
 		number=$((number / i))
 	done
 done
