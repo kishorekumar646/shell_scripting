8b945d2568d1d668eefd98efdfca29e129f5c43e
kishorekumar646 <kishorekumar131646@gmail.com>
2020-02-11 09:01:49 +0530

    [kishore]Refactor.refactor add comment in headder

--- week1/Core_programs/user_name.sh
+++ week1/Core_programs/user_name.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+<<comment
+	Replace user name in given a string
+comment
+
 user="Hello <<UserName>>, How are you?"
 read -p "Enter your name : " user_name
 echo "${user/<<UserName>>/$user_name}"
