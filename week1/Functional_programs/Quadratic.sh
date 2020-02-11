6ff9414e17efe4e44b9881290b3137afddeab617
kishorekumar646 <kishorekumar131646@gmail.com>
2020-02-11 10:54:31 +0530

    [kishore]Refactor.refactor adding comment

--- week1/Functional_programs/Quadratic.sh
+++ week1/Functional_programs/Quadratic.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+<<comment
+	
+	find the roots of the equation a*x*x + b*x + c. 
+	
+	delta = b*b - 4*a*c
+	Root 1 of x = (-b + sqrt(delta))/(2*a)
+	Root 2 of x = (-b - sqrt(delta))/(2*a)
+
+comment
+
 quadratice_equation()
 {
 	delta=$(echo "scale=3; $((b**2 - 4*a*c))" | bc -l)
@@ -7,6 +17,9 @@ quadratice_equation()
 
 	root1=$(echo "$((-b + $(echo "scale=3;sqrt ( $((delta / 2 * a)) )" |bc -l )))" | bc -l)
 	echo "$root1"
+
+	root2=$(echo "$((-b - $(echo "scale=3;sqrt ( $((delta / 2 * a)) )" |bc -l )))" | bc -l)
+	echo "$root2"
 }
 
 read -p "Enter a : " a
