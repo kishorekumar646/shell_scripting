3206c225de1b0bf90c3094e03633335d07e8816b
kishorekumar646 <kishorekumar131646@gmail.com>
2020-02-11 10:49:49 +0530

    [kishore]Refactor.refactor variable naming convention

--- week1/Functional_programs/Distance.sh
+++ week1/Functional_programs/Distance.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+<<comment
+	susing this formula
+	calculate distance = sqrt(x*x + y*y)
+comment
+
 distance()
 {
-	c=$(echo "scale=3;sqrt ( $((x ** 2 + y ** 2)) )" | bc -l)
-	echo "$c"
+	distance_value=$(echo "scale=3;sqrt ( $((x_axis ** 2 + y_axis ** 2)) )" | bc -l)
+	echo "$distance_value"
 }
 
-read -p "Value of x : " x
-read -p "Value of y : " y
+read -p "Value of x : " x_axis
+read -p "Value of y : " y_axis
 
-distance $x $y
+distance $x_axis $y_axis
